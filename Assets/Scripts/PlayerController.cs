6bb33d0a9c664d6912d1456f2c5526aa6d826ef1
zheng <zxiong@email.sc.edu>
2017-07-10 12:44:51 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    private int count;  // count collectible objects
+
+    public Text countText;  // reference to count text component
+
+    public Text winText;
+
+    //  init, often 1st scene of the game
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text= "";
+
+    }
+
+    //  Physics code is here
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); // y=0, no vertical movement
+
+        rb.AddForce(movement * speed);
+    }
+
+
+    //  Detect collision and remove cube from board.
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Won!";
+        }
+    }
+
+}
